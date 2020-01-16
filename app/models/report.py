98ad213bffa82f0f740842ab2eb23e9139a166b1
何敏 <heminitao@minMacBook.local>
2020-01-16 14:42:34 +0800

    init

--- app/models/report.py
+++ app/models/report.py
@@ -4,9 +4,9 @@ from app.models import db
 class Report(db.Model):
     __tablename__ = 'report'
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    mg_id = db.Columns(db.String(50), nullable=False)
-    req_mg = db.Columns(db.String(50), nullable=False)
-    rep_item = db.Columns(db.String(50), nullable=False)
-    okr = db.Columns(db.String(50), nullable=False)
+    mg_id = db.Column(db.String(50), nullable=False)
+    req_mg = db.Column(db.String(50), nullable=False)
+    rep_item = db.Column(db.String(50), nullable=False)
+    okr = db.Column(db.String(50), nullable=False)
 
 
