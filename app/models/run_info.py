fa5b549a776273fffaa8a2a8aabae07a740efa59
hexiaominitao <a8013heming@gmail.com>
2020-04-29 14:07:48 +0800

    init

--- app/models/run_info.py
+++ app/models/run_info.py
@@ -24,6 +24,8 @@ class SeqInfo(db.Model):
     item = db.Column(db.String(255))  # 检测项目
     barcode = db.Column(db.String(255))  # Barcode编号
     note = db.Column(db.String(255))  # 备注
+    cancer = db.Column(db.String(255)) # 报告用癌症类型
+    report_item = db.Column(db.String(255)) # 报告模板类型
     status = db.Column(db.String(255))  # 状态
     run_info_id = db.Column(db.Integer(), db.ForeignKey('run_info.id'))
     sample_info_id = db.Column(db.Integer(), db.ForeignKey('sample_info.id'))
@@ -33,6 +35,10 @@ class SeqInfo(db.Model):
             'id': self.id, 'sample_name': self.sample_name,
             'sample_mg': self.sample_mg, 'item': self.item,
             'barcode': self.barcode, 'note': self.note,
-            'status': self.status
+            'status': self.status, 'cancer': self.cancer,
+            'report_item': self.report_item
         }
+        for k, v in my_dict.items():
+            if not v:
+                my_dict[k] = ''
         return my_dict
