468deaf635187f58e99487c8761f4546dae89322
hexiaominitao <a8013heming@gmail.com>
2020-05-14 15:10:37 +0800

    fix:添加原始数据下载3

--- app/libs/report.py
+++ app/libs/report.py
@@ -501,12 +501,13 @@ def get_raw_file(seq):
             continue
         for root, paths, files in os.walk(os.path.join(path_result, path_run)):
             for file in files:
-                if file == '{}.results.xls'.format(seq.sample_name):
-                    dict_result['result'] = (os.path.join(root, file))
-                if file == '{}.raw.bam'.format(seq.sample_name):
-                    dict_result['bam'] = (os.path.join(root, file))
-                if file == '{}.raw.bai'.format(seq.sample_name):
-                    dict_result['bai'] = (os.path.join(root, file))
+                if seq.sample_name in file:
+                    if file == '{}.results.xls'.format(seq.sample_name):
+                        dict_result['result'] = (os.path.join(root, file))
+                    if file.endswith('.raw.bam'):
+                        dict_result['bam'] = (os.path.join(root, file))
+                    if file.endswith('.raw.bam.bai'):
+                        dict_result['bai'] = (os.path.join(root, file))
     return dict_result
 
 def get_result_file(seq, key):
